4f9ec201b0fc1f51f4c5bf04935dbb6f86b928d5
TuanPP <phuongtuan1803@gmail.com>
2022-04-30 21:54:22 +0700

    update new test

--- projects/codejam_202204/codejam_A.cpp
+++ projects/codejam_202204/codejam_A.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 
-#define _DEBUG
-//#undef _DEBUG
+// #define _DEBUG
+#undef _DEBUG
 
 using namespace std;
 
@@ -31,126 +31,148 @@ ifstream ifile(_TESTCASEFILE_, ifstream::in);
 /* GLOBAL VAR */
 char c[150];
 
-vector<int> subset;
 int alice_first_choice = 0;
 int alice_second_choice = 0;
-
-void search(int k, int n, vector<int> &bob_v, vector<int> &alice_v)
+int bob_choice = 0;
+vector<int> permutation;
+vector<int> alice_first_choice_v;
+void search(int n, vector<bool>& chosen , vector<int>& alice_v,vector<int>& bob_v)
 {
-        if (k == n)
+    if (permutation.size() == n)
+    {
+        // cout << "    n: " << permutation.at(0) << '\n';
+        // process permutation
+        int tmp = 0;
+        for (int i = 0; i < permutation.size() ; i++)
         {
-            // process subset
-            for (int i = 0; i < subset.size(); i++)
-            {
-                alice_first_choice = alice_first_choice * 10 + subset.at(i);
-            }
-            cout << "    -----: " << alice_first_choice << '\n';
+            tmp = tmp * 10 + permutation.at(i);
         }
-        else
+        if(tmp > alice_first_choice && tmp < bob_choice){
+            alice_first_choice = tmp;
+            // cout << "    tmp2: " << tmp << '\n';
+            return;
+            // alice_first_choice_v.assign(permutation.begin(), permutation.end());
+        }
+        // cout << "    tmp: " << tmp << '\n';
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
         {
-            // include k in the subset
-            subset.push_back(alice_v.at(k));
-            cout << "    k    : " << k << '\n';
-            cout << "    -----: " << bob_v.at(k) << '\n';
-            cout << "    -----: " << alice_v.at(k) << '\n';
-
-            if (bob_v.at(k) >= alice_v.at(k))
-            {
-                search(k, n, bob_v, alice_v);
-            }
-            subset.pop_back();
-            // don’t include k in the subset
-            if (bob_v.at(k) >= alice_v.at(k))
-            {
-                search(k, n, bob_v, alice_v);
-            }
+            // cout << "    i: " << i << "    alice_v.at(i): " << alice_v.at(i) << '\n';
+            // cout << "    bob_v.at(i): " << bob_v.at(i) << "    alice_v.at(i): " << alice_v.at(i) << '\n';
+            // if(alice_v.at(i) > bob_v.at(i) ){
+            //     continue;
+            // }
+            // if(alice_first_choice_v.size() == n ){
+            //     cout << "    alice_first_choice_v.at(i): " << alice_first_choice_v.at(i) << "    alice_v.at(i): " << alice_v.at(i) << '\n';
+            //     if( alice_v.at(i) < alice_first_choice_v.at(i)){
+                    
+            //         continue;
+            //     }                
+            // }
+            if (chosen[i])
+                continue;
+            
+            chosen[i] = true;
+            permutation.push_back(alice_v.at(i));
+            // if(alice_v.at(i) <= bob_v.at(i) ){
+                search(n,chosen,alice_v,bob_v);
+            // }
+            
+            chosen[i] = false;
+            permutation.pop_back();
         }
+    }
 }
 
 int main()
 {
-        _INIT_();
-        /*  INPUT*/
-        int T;
-        cinstream >> T;
+    _INIT_();
+    /*  INPUT*/
+    int T;
+    cinstream >> T;
 
 #ifdef _DEBUG
-        printf("Loading Testcase from file: %s (Number of TestCase: %d )\n", _TESTCASEFILE_.c_str(), T);
-        printf("=================================\n");
+    printf("Loading Testcase from file: %s (Number of TestCase: %d )\n", _TESTCASEFILE_.c_str(), T);
+    printf("=================================\n");
 #endif
 
-        _START_TIMESTAMP_();
+    _START_TIMESTAMP_();
 
-        for (int i = 0; i < T - 2; i++)
+    for (int i = 0; i < T; i++)
+    {
+        // printf("=================================\n");
+        permutation.clear();
+        int n, bob_number, alice_number;
+        int alice_choice;
+        cinstream >> n >> bob_number >> alice_number;
+
+        int tmp;
+        // code here
+        // Get vector of alice
+        vector<int> alice_v;
+        tmp = alice_number;
+        while (tmp != 0)
         {
-            subset.clear();
-            int n, bob_number, alice_number;
-            int alice_choice;
-            cinstream >> n >> bob_number >> alice_number;
-
-            int tmp;
-            // code here
-            // Get vector of alice
-            vector<int> alice_v;
-            tmp = alice_number;
-            while (tmp != 0)
-            {
-                alice_v.push_back(tmp % 10);
-                tmp = tmp / 10;
-            }
-
-            // Get min choise of bob
-            int bob_choice, bob_number_reverse = 0;
-            tmp = bob_number;
-            while (tmp != 0)
-            {
-                bob_number_reverse = bob_number_reverse * 10 + tmp % 10;
-                tmp = tmp / 10;
-            }
-            bob_choice = bob_number < bob_number_reverse ? bob_number : bob_number_reverse;
-
-            // Get vector of bob
-            vector<int> bob_v;
-            tmp = bob_choice;
-            while (tmp != 0)
-            {
-                bob_v.push_back(tmp % 10);
-                tmp = tmp / 10;
-            }
-
-        // Alice last choise
-        std:
-            sort(alice_v.begin(), alice_v.end(), std::greater<int>());
-            alice_second_choice = 0;
-            for (int i = 0; i < alice_v.size() - 1; i++)
-            {
-                alice_second_choice = alice_second_choice * 10 + alice_v.at(i);
-            }
-
-            // Alice first choise
-            int alice_final_choice = 0;
-            bool chosen[n + 1];
-            alice_first_choice = -1;
-            search(0, n, bob_v, alice_v);
-            if (alice_first_choice == -1)
-            {
-                alice_final_choice = alice_second_choice;
-            }
-            else
-            {
-                alice_final_choice = alice_first_choice;
-            }
-#ifdef _DEBUG
-            cout << "testcase: n = " << n << ", bob_number: " << bob_number << ", alice_number: " << alice_number << '\n';
-            cout << "alice_final_choice: " << alice_final_choice << '\n';
-            cout << "    alice_second_choice: " << alice_second_choice << '\n';
-            cout << "    alice_first_choice: " << alice_first_choice << '\n';
-            cout << "    bob_choice: " << bob_choice << '\n';
-#else
-            cout << alice_final_choice << '\n';
-#endif
+            alice_v.push_back(tmp % 10);
+            tmp = tmp / 10;
         }
 
-        _STOP_TIMESTAMP_();
-        return 0;
+        // Get min choise of bob
+        int bob_number_reverse = 0;
+        tmp = bob_number;
+        while (tmp != 0)
+        {
+            bob_number_reverse = bob_number_reverse * 10 + tmp % 10;
+            tmp = tmp / 10;
+        }
+        bob_choice = bob_number < bob_number_reverse ? bob_number : bob_number_reverse;
+
+        // Get vector of bob
+        vector<int> bob_v;
+        tmp = bob_choice;
+        while (tmp != 0)
+        {
+            bob_v.push_back(tmp % 10);
+            tmp = tmp / 10;
+        }
+
+    // Alice last choise
+    
+        std::sort(alice_v.begin(), alice_v.end(), std::greater<int>());
+        alice_second_choice = 0;
+        for (int i = 0; i < alice_v.size() - 1; i++)
+        {
+            alice_second_choice = alice_second_choice * 10 + alice_v.at(i);
+        }
+
+        // Alice first choise
+        int alice_final_choice = 0;
+        vector<bool> chosen (n,false);
+        alice_first_choice = -1;
+        // std::sort(alice_v.begin(), alice_v.end());
+        search(n,chosen,alice_v,bob_v);
+        
+        if (alice_first_choice == -1)
+        {
+            alice_final_choice = alice_second_choice;
+        }
+        else
+        {
+            alice_final_choice = alice_first_choice;
+        }
+#ifdef _DEBUG
+        cout << "testcase: n = " << n << ", bob_number: " << bob_number << ", alice_number: " << alice_number << '\n';
+        cout << "alice_final_choice: " << alice_final_choice << '\n';
+        cout << "    alice_second_choice: " << alice_second_choice << '\n';
+        cout << "    alice_first_choice: " << alice_first_choice << '\n';
+        cout << "    bob_choice: " << bob_choice << '\n';
+#else
+        cout << alice_final_choice << '\n';
+#endif
+    }
+
+    _STOP_TIMESTAMP_();
+    return 0;
 }
---
+++ projects/codejam_202204/codejam_B.cpp
@@ -0,0 +1,216 @@
+#include <bits/stdc++.h>
+
+#define _DEBUG
+// #undef _DEBUG
+
+using namespace std;
+
+/*  ====== PRE DEFINE ================================================================================================================ */
+#ifdef _DEBUG
+#define _INIT_()
+#define _START_TIMESTAMP_() auto start = chrono::steady_clock::now();
+#define _STOP_TIMESTAMP_() cout << "Elapsed time in milliseconds: "                                                         \
+                                << chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start).count() \
+                                << " ms" << '\n';
+
+#define _TESTCASEFILE_ (string(__FILE__).substr(0, string(__FILE__).length() - 4) + ".txt")
+ifstream ifile(_TESTCASEFILE_, ifstream::in);
+#define cinstream ifile
+
+#else
+#define _INIT_()                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define _START_TIMESTAMP_()
+#define _STOP_TIMESTAMP_()
+
+#define cinstream cin
+#endif
+/*  ================================================================================================================================== */
+
+/* GLOBAL VAR */
+
+long long largest_number_wei[50001]; 
+int size_wei;
+long long n, m;
+long long A[50001];
+long long A_count[50001];
+int x_start[50001];
+int y_stop[50001];
+long long min_index;
+long long x[200001];
+long long y[200001];
+long long max_value = 0;
+long long times = 0;
+long long giaithuaarray[50001];
+
+void tinhtruocgiaithua(int max)
+{
+    for (int x = 0; x < max; x++)
+    {
+        if (giaithuaarray[x] != 0)
+            continue;
+        if (x == 0 || x == 1)
+            giaithuaarray[x] = 1;
+        else
+            giaithuaarray[x] = (x * giaithuaarray[x - 1]) % 1000000007;
+    }
+}
+
+long long giaiThua(int x)
+{
+    // cout << "            giaithuaarray[x] " << x  << '\n';
+    if (giaithuaarray[x] != 0)
+    {
+        return giaithuaarray[x];
+    }
+
+    if (x == 0 || x == 1)
+        return 1;
+
+    giaithuaarray[x] = (x % 1000000007* giaiThua(x - 1)% 1000000007)  % 1000000007;
+    return giaithuaarray[x];
+}
+
+long long tohopdongcap()
+{
+    long long ret_val = 1;
+    if (size_wei == 1)
+    {
+        return ret_val;
+    }
+    int count = 0;
+    for (int i = 1; i < size_wei; i++)
+    {
+        if (largest_number_wei[i - 1] == largest_number_wei[i])
+        {
+            if (i == 1)
+            {
+                count = 2;
+            }
+            else
+            {
+                count++;
+            }
+            if (i == size_wei)
+            {
+            }
+        }
+        else
+        {
+            if (i == 1)
+            {
+                count = 1;
+            }
+            ret_val *= giaiThua(count);
+            count = 1;
+        }
+    }
+    if (count > 0)
+    {
+        ret_val *= giaiThua(count);
+    }
+    return ret_val;
+}
+
+int main()
+{
+    _INIT_();
+    /*  INPUT*/
+    long long T;
+    cinstream >> T;
+
+#ifdef _DEBUG
+    printf("Loading Testcase from file: %s (Number of TestCase: %d )\n", _TESTCASEFILE_.c_str(), T);
+    printf("=================================\n");
+#endif
+
+    _START_TIMESTAMP_();
+    tinhtruocgiaithua(50000);
+    for (int tc = 0; tc < T; tc++)
+    {
+// #ifdef _DEBUG
+//         printf("\n=================================\n");
+// #endif
+        max_value = -1;
+        times = 0;
+        long long tmp;
+
+        cinstream >> n >> m;
+
+        std::fill_n(A, n, 0);
+        std::fill_n(A_count, n, 0);
+
+        std::fill_n(largest_number_wei, n, 0);
+        std::fill_n(x, m, 0);
+        std::fill_n(y, m, 0);
+        std::fill_n(x_start, n, false);
+        std::fill_n(y_stop, n, false);
+
+        for (int i = 0; i < n; i++)
+        {
+            cinstream >> A[i];
+        }
+
+        
+        for (int i = 0; i < m; i++)
+        {
+            cinstream >> x[i];
+            cinstream >> y[i];
+            x_start[x[i] - 1] = x_start[x[i] - 1] + 1;
+            y_stop[y[i] - 1] = y_stop[y[i] - 1] + 1;
+            
+        }
+
+        // code here
+        int incre = 0;
+        for (int i = 0; i < n; i++)
+        {
+
+            if(x_start[i] !=0 ) incre+=x_start[i] ;
+            A_count[i] = incre;
+            if(y_stop[i] !=0  ) incre-=y_stop[i] ;
+        }
+
+        // for (int i = 0; i < m; i++)
+        // {
+        //     for (int j = x[i] - 1; j <= y[i] - 1; j++)
+        //     {
+        //         A_count[j]++;
+        //     }
+        // }
+
+        size_wei = 0;
+        for (int i = 0; i < n; i++)
+        {
+            // cout << "A_count[i] " << A_count[i]<< '\n';
+            // cout << "x_start[i] " << x_start[i]<< '\n';
+            // cout << "y_stop[i] " << y_stop[i]<< '\n';
+            if(A_count[i] != 0)
+                largest_number_wei[size_wei++] = A_count[i];
+        }
+        // for(int i = 0 ; i < size_wei ; i++){
+        //     cout << "largest_number_wei[i] " << largest_number_wei[i] << '\n';
+        // }
+        std::sort(A, A+n, std::greater<long long>());
+        std::sort(largest_number_wei, largest_number_wei+size_wei, std::greater<long long>());
+
+        max_value = 0;
+        for (int j = 0; j < size_wei; j++)
+        {
+            max_value += A[j] * largest_number_wei[j];
+        }
+    
+        times = (tohopdongcap() * giaiThua(n - size_wei)) % 1000000007;
+
+#ifdef _DEBUG
+        // cout << "testcase: n = " << n << ", m: " << m   << '\n';
+        cout << max_value << " " << times << '\n';
+#else
+        cout << max_value << " " << times << '\n';
+#endif
+    }
+
+    _STOP_TIMESTAMP_();
+    return 0;
+}
